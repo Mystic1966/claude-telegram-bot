c1f7083ad2267b8e1989c093b747c1dfc770425a
Mystic1966 <117193380+Mystic1966@users.noreply.github.com>
2026-04-19 14:39:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ bot.on("message", async (msg) => {
   try {
     const command = new InvokeModelCommand({
       // ✅ YOUR EXACT MODEL
-      modelId: "anthropic.claude-haiku-4-5-20251001-v1:0",
+      modelId: "anthropic.claude-3-haiku-20240307",
 
       contentType: "application/json",
       accept: "application/json",
