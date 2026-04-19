f93439390f491998403d97ed312d1efc5df3f551
Mystic1966 <117193380+Mystic1966@users.noreply.github.com>
2026-04-19 14:35:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ bot.on("message", async (msg) => {
   try {
     const command = new InvokeModelCommand({
       // ✅ SAFE MODEL (works in most accounts)
-     modelId: "anthropic.claude-3-sonnet-20240229",
+     modelId: "anthropic.claude-3-5-haiku-20241022-v1:0",
 
       contentType: "application/json",
       accept: "application/json",
